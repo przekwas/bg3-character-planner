ac3319c38e3f082df25a29f9db9cc3f11dd33b4d
Luke Przkwas <luke@covalence.io>
2023-11-29 14:41:10 -0600

    adding code at end of video two

--- server/characterService.js
+++ server/characterService.js
@@ -3,6 +3,11 @@ const path = require('path');
 
 const DATA_PATH = path.join(__dirname, './data/character.json');
 
+const INITIAL_DATA = {
+	totalLevels: 0,
+	classes: []
+};
+
 function readData() {
 	return new Promise((resolve, reject) => {
 		fs.readFile(DATA_PATH, (err, data) => {
@@ -21,6 +26,48 @@ function readData() {
 	});
 }
 
-module.exports = {
-    readData
+function writeData(data) {
+	return new Promise((resolve, reject) => {
+		fs.writeFile(DATA_PATH, JSON.stringify(data, null, 2), err => {
+			if (err) {
+				reject(err);
+				return;
+			}
+
+			resolve('Write file successful');
+		});
+	});
 }
+
+function checkDataFile() {
+	return new Promise((resolve, reject) => {
+		fs.access(DATA_PATH, fs.constants.F_OK, err => {
+			if (err) {
+				writeData(INITIAL_DATA)
+					.then(() => resolve('Data file created'))
+					.catch(reject);
+			} else {
+				fs.readFile(DATA_PATH, (readErr, data) => {
+					if (readErr) {
+						reject(readErr);
+						return;
+					}
+
+					if (data.length === 0) {
+						writeData(INITIAL_DATA)
+							.then(() => resolve('Data file created'))
+							.catch(reject);
+					} else {
+						resolve('Data file is valid');
+					}
+				});
+			}
+		});
+	});
+}
+
+module.exports = {
+	readData,
+	writeData,
+	checkDataFile
+};
--- server/server.js
+++ server/server.js
@@ -9,10 +9,12 @@ app.use((req, res, next) => {
 	next();
 });
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 // GET http://localhost:3000/character
 app.get('/character', async (req, res) => {
 	try {
+		await characterService.checkDataFile();
 		const characterData = await characterService.readData();
 		res.json(characterData);
 	} catch (error) {
@@ -21,8 +23,38 @@ app.get('/character', async (req, res) => {
 });
 
 // POST http://localhost:3000/levelup
-app.post('/levelup', (req, res) => {
-	res.send('Level up');
+// body urlencoded { selectedClass: string }
+app.post('/levelup', async (req, res) => {
+	try {
+		const selectedClass = req.body.selectedClass;
+
+		if (!selectedClass) {
+			return res
+				.status(400)
+				.json({ message: 'Level up failed', error: 'Invalid input' });
+		}
+
+		const characterData = await characterService.readData();
+
+		if (characterData.totalLevels >= 12) {
+			return res
+				.status(400)
+				.json({ message: 'Level up failed', error: 'Level limit reached' });
+		}
+
+		characterData.totalLevels += 1;
+
+		characterData.classes.push({
+			level: characterData.totalLevels,
+			class: selectedClass
+		});
+
+		await characterService.writeData(characterData);
+
+		res.json({ message: 'Level up successful' });
+	} catch (error) {
+		res.status(500).json({ error: 'Oops my code sucks', message: error.message });
+	}
 });
 
 const PORT = process.env.PORT || 3000;
